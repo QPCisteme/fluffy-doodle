6e4d65d00618802c6a6a2183f438fcc2ecf045a2
unknown <perroche@cisteme.net>
2025-09-23 12:01:04 +0200

    added M7-SPI delay

--- cisteme_mpl460a.c
+++ cisteme_mpl460a.c
@@ -218,6 +218,8 @@ static int start_fw(const struct device *dev)
     if (ret < 0)
         return ret;
 
+    k_msleep(10);
+
     // Give MISO to M7-SPI
     ret = boot_write(dev, 0, PL460_MISO_M7_NCLK, 0, 0);
     if (ret < 0)
