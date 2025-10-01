4a6ca2d7521d7cafd5a145cb716cfc9b21e09e17
unknown <perroche@cisteme.net>
2025-10-01 09:32:19 +0200

    disabled int

--- cisteme_mpl460a.c
+++ cisteme_mpl460a.c
@@ -207,17 +207,11 @@ static int boot_enable(const struct device *dev)
 
 static int boot_disable(const struct device *dev)
 {
-
     int ret;
-
-    // Union to reverse endianess
-    union {
-        uint32_t val;
-        uint8_t tab[4];
-    } mpl460a_conv;
+    uint8_t tab[4];
 
     // Clean CPUWAIT to start program
-    mpl460a_conv.val = 0x01010000;
+    sys_put_le32(tab, 0x01010000);
     ret = boot_write(dev, PL460_MISCR, PL460_WR, mpl460a_conv.tab, 4);
     if (ret < 0)
         return ret;
@@ -442,8 +436,7 @@ static int mpl460a_init(const struct device *dev)
     drv_data->params.rs2Blocks = 0;
     drv_data->params.delimiterType = 0;
 
-    gpio_pin_interrupt_configure_dt(&drv_config->extin,
-                                    GPIO_INT_EDGE_TO_ACTIVE);
+    gpio_pin_interrupt_configure_dt(&drv_config->extin, GPIO_INT_INACTIVE);
     gpio_init_callback(&drv_data->extin_cb_data, extin_IRQ,
                        BIT(drv_config->extin.pin));
     gpio_add_callback(drv_config->extin.port, &drv_data->extin_cb_data);
