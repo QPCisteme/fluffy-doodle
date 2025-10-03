7abee5ecb40841e14b23eef6faa95c6eb5378bf2
unknown <perroche@cisteme.net>
2025-10-03 14:58:59 +0200

    forced falling edfe

--- cisteme_mpl460a.c
+++ cisteme_mpl460a.c
@@ -345,8 +345,7 @@ static int fw_send(const struct device *dev, uint16_t *data, uint8_t len)
 
     gpio_pin_set_dt(&drv_config->txen, 1);
 
-    gpio_pin_interrupt_configure_dt(&drv_config->extin,
-                                    GPIO_INT_EDGE_TO_ACTIVE);
+    gpio_pin_interrupt_configure_dt(&drv_config->extin, GPIO_INT_EDGE_FALLING);
     gpio_init_callback(&drv_data->extin_cb_data, extin_IRQ,
                        BIT(drv_config->extin.pin));
     gpio_add_callback(drv_config->extin.port, &drv_data->extin_cb_data);
