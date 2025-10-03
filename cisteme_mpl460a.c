2c4641358d8ee93e0339adb5f97b87a8f147e2f1
unknown <perroche@cisteme.net>
2025-10-03 14:39:42 +0200

    interrupt enabled sooner

--- cisteme_mpl460a.c
+++ cisteme_mpl460a.c
@@ -345,6 +345,12 @@ static int fw_send(const struct device *dev, uint16_t *data, uint8_t len)
 
     gpio_pin_set_dt(&drv_config->txen, 1);
 
+    gpio_pin_interrupt_configure_dt(&drv_config->extin,
+                                    GPIO_INT_EDGE_TO_ACTIVE);
+    gpio_init_callback(&drv_data->extin_cb_data, extin_IRQ,
+                       BIT(drv_config->extin.pin));
+    gpio_add_callback(drv_config->extin.port, &drv_data->extin_cb_data);
+
     drv_data->params.dataLength = len << 1;
     uint16_t tx_params[20];
 
@@ -358,12 +364,6 @@ static int fw_send(const struct device *dev, uint16_t *data, uint8_t len)
     ret = fw_id_send(dev, PL460_G3_TX_PARAM, tx_params, 20, 0, 0, true);
     if (ret < 0)
         return ret;
-
-    gpio_pin_interrupt_configure_dt(&drv_config->extin,
-                                    GPIO_INT_EDGE_TO_ACTIVE);
-    gpio_init_callback(&drv_data->extin_cb_data, extin_IRQ,
-                       BIT(drv_config->extin.pin));
-    gpio_add_callback(drv_config->extin.port, &drv_data->extin_cb_data);
 
     // CMD 2 : données
     ret = fw_id_send(dev, PL460_G3_TX_DATA, data, len, 0, 0, true);
