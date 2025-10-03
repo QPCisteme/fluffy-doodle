610047b7872ec39ad8bf35d76868591fe70c3056
unknown <perroche@cisteme.net>
2025-10-03 15:56:48 +0200

    enable irq before txen ?

--- cisteme_mpl460a.c
+++ cisteme_mpl460a.c
@@ -350,6 +350,9 @@ static int fw_send(const struct device *dev, uint16_t *data, uint8_t len)
 
     int ret;
 
+    gpio_pin_interrupt_configure_dt(&drv_config->extin,
+                                    GPIO_INT_EDGE_TO_ACTIVE);
+
     gpio_pin_set_dt(&drv_config->txen, 1);
 
     drv_data->params.dataLength = len << 1;
@@ -365,8 +368,6 @@ static int fw_send(const struct device *dev, uint16_t *data, uint8_t len)
     ret = fw_id_send(dev, PL460_G3_TX_PARAM, tx_params, 20, 0, 0, true);
     if (ret < 0)
         return ret;
-
-    gpio_pin_interrupt_configure_dt(&drv_config->extin, GPIO_INT_EDGE_FALLING);
 
     // CMD 2 : données
     ret = fw_id_send(dev, PL460_G3_TX_DATA, data, len, 0, 0, true);
